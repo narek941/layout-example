d220025c0cb0193271ecb4a9a2b3b55d48690430
narek941 <nqolyan@gmail.com>
2020-11-25 12:19:29 +0000

    add footer

--- src/components/common/footer/Footer.jsx
+++ src/components/common/footer/Footer.jsx
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-
+import styles from './Footer.css';
 
 const Footer = () => {
       return (
-            <div>
+            <div className="footer">
                   <ul>
                         <li>
                               <Link to="/about">
